fd75f1dd2e93506bc959a2083642f9ec46b67339
Nazar20000 <normanprogramer76gmail.com>
2025-07-17 18:21:04 +0300

    Фронтент

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -2,7 +2,7 @@
 <header class="header">
     <div class="container">
         <div class="logo">
-            <span class="logo-text">BUCKET<br>BUDDY</span>
+            <span class="logo-text">D.N B<br>Motors V</span>
             <span class="logo-subtext">auto llc</span>
         </div>
         <nav class="nav">
